8067a94614bce24721603f1652d2163b7071dd6f
obi123 <oasinu24@gmail.com>
2016-11-29 20:44:10 -0500

    initialized all java files

---
+++ DailyPlayerStats.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * This class is responsible for tracking and returning information regarding a player's daily stats 
+ * @author obinnaasinugo
+ *
+ */
+public class DailyPlayerStats {
+
+	String player; 
+	
+	public DailyPlayerStats(String player){
+		this.player = player; 
+	}
+}
---
+++ FullGameSchedule.java
@@ -0,0 +1,11 @@
+package hw6_project;
+
+/**
+ * This class is responsible for tracking information pertaining to the full schedule 
+ * of a particular team. 
+ * @author obinnaasinugo
+ *
+ */
+public class FullGameSchedule {
+
+}
