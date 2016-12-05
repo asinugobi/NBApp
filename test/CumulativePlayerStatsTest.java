d4c24a736b65c0d1f043cca8696b3f990d4c842f
Emmanuel Chimezie <echimezie@getvitaminc.com>
2016-12-05 01:43:51 -0500

    pushing the actual files for cumulativeplayerstats class

---
+++ test/CumulativePlayerStatsTest.java
@@ -0,0 +1,43 @@
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CumulativePlayerStatsTest {
+
+	CumulativePlayerStats stats; // initialize object
+
+	@Before
+	public void setUp(){
+		stats = new CumulativePlayerStats();
+	}
+
+	//@Test
+	// public void testMakeDataMap() {
+	// 	HashMap<String, String[]> playerStats = stats.getPlayersStatsMap();
+	//
+	// 	for(String player : playerStats.keySet()){
+	// 		System.out.println(player + ": " + Arrays.toString(playerStats.get(player)));
+	// 	}
+	// }
+
+	@Test
+	public void getStatsTest(){
+		//SortedSet<Double> pointsSorted = stats.getStats();
+		// Iterator it = pointsSorted.iterator();
+		// while(it.hasNext()){
+		// 	System.out.println(element.getIvar1());
+		// }
+		//stats.getStats();
+	}
+	
+	@Test
+	public void testGetStatCategory(){
+		int num = stats.getStatCategory("points");
+		assertEquals("The column for points is",47, num);
+	}
+
+}
