da31b6dbf38e34377489a7a187d76caab266107e
Emmanuel Chimezie <echimezie@getvitaminc.com>
2016-12-06 12:44:23 -0500

    updated cumulative player stats test

---
+++ test/DailyPlayerStatsTest2.java
@@ -0,0 +1,30 @@
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DailyPlayerStatsTest2 {
+
+	DailyPlayerStats stats; // initialize object 
+	
+	@Before 
+	public void setUp(){
+		stats = new DailyPlayerStats(); 
+	}
+	
+	@Test
+	public void testMakeDataMap() {
+		HashMap<String, String[]> playerStats = stats.getPlayersStatsMap();
+		
+		for(String player : playerStats.keySet()){
+			System.out.println(player + ": " + Arrays.toString(playerStats.get(player)));
+		}
+		
+		
+	}
+
+}
