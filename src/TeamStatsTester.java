253a8ff38d13d0d8561f8634608ca41fd14a89f2
Ying Tsai <Eddie@Yings-MacBook-Pro.local>
2016-12-07 00:24:13 -0500

    Update the PlayerInjuries java file that can get target injured players

--- src/TeamStatsTester.java
+++ src/TeamStatsTester.java
@@ -6,8 +6,12 @@ public class TeamStatsTester {
 
 	public static void main(String[] args) throws MalformedURLException, IOException {
 		// TODO Auto-generated method stub
+		
+		PlayerInjuries injury = new PlayerInjuries("Al Horford", "Knicks");
+		System.out.println(injury.getPlayer());
+		System.out.println(injury.getTeam());
+		
 		TeamStats team = new TeamStats("spurs");
-
 		System.out.println(team.getTop5Scores());
 		System.out.println(team.getTop5Rebounds());
 		System.out.println(team.getTop5Assists());
