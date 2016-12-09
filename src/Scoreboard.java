eae2d8bd195682bd408cf085905236f6bd894985
Emmanuel Chimezie <echimezie@getvitaminc.com>
2016-12-08 23:13:11 -0500

    initial edits to scoreboard file'

--- src/Scoreboard.java
+++ src/Scoreboard.java
@@ -1,16 +1,16 @@
 
 
 /**
- * This class is responsible for tracking  the score of particular 
- * game being played. 
- * @author obinnaasinugo
+ * This class is responsible for tracking  the score of particular
+ * game being played.
+ * @author chimezie
  *
  */
 public class Scoreboard {
 
-	private String team; // team of interest 
-	
+	private String team; // team of interest
+
 	public Scoreboard(String team){
-		this.team = team; 
+		this.team = team;
 	}
 }
