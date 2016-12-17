be13c71b46e5d83d4ac548dc9e3bc8aeadede76e
Emmanuel Chimezie <echimezie@getvitaminc.com>
2016-12-17 15:48:55 -0500

    team standings updates

--- src/TeamStandings.java
+++ src/TeamStandings.java
@@ -3,60 +3,60 @@
 import java.util.HashMap;
 
 /**
- * This class is responsible for evaluating data pertaining to 
- * the overall standings, conference team standings, division 
- * team standings, and playoff team standings. 
+ * This class is responsible for evaluating data pertaining to
+ * the overall standings, conference team standings, division
+ * team standings, and playoff team standings.
  * @author obinnaasinugo
  *
  */
 public class TeamStandings {
-	
-	private FileReaderv3 teamStandings; 
-	private HashMap<Integer, String[]> teamStatsMap; 
-	
+
+	private FileReaderv3 teamStandings;
+	private HashMap<Integer, String[]> teamStatsMap;
+
 	/**
-	 * 
+	 *
 	 */
 	public TeamStandings(){
 		teamStandings = new FileReaderv3("MYSPORTSFEEDS-OVERALL_TEAM_STANDINGS-NBA-20152016REGULAR.csv");
 		teamStatsMap = new HashMap<Integer, String[]>();
-		makeTeamStandingsMap(); 
+		makeTeamStandingsMap();
 	}
-	
+
 	/**
-	 * This method will return the file read in by the program 
-	 * @return file read in by the program 
+	 * This method will return the file read in by the program
+	 * @return file read in by the program
 	 */
 	public FileReaderv3 getFile(){
 		return teamStandings;
 	}
-	
+
 	/**
-	 * This method will populate the team standings map. The key is the rank of the team; the 
+	 * This method will populate the team standings map. The key is the rank of the team; the
 	 * value is the team info (name, record, etc.)
 	 */
 	public void makeTeamStandingsMap(){
-		String[] teamInfo = null; 
-		
+		String[] teamInfo = null;
+
 		for(String team : teamStandings.getLines()){
-			teamInfo = team.split(","); 
+			teamInfo = team.split(",");
 			teamStatsMap.put(Integer.parseInt(teamInfo[5]), teamInfo);
 		}
 	}
-	
-	
+
+
 	/**
 	 * This method will take in the rank of a team and return the team's info (name, record, etc.)
 	 * @param rank
-	 * @return team's info as an array 
+	 * @return team's info as an array
 	 */
 	public String[] getTeamInfo(int rank){
 		return teamStatsMap.get(rank);
 	}
-	
-	
-	
-	
-	
-		 
+
+
+
+
+
+
 }
